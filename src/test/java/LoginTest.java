6244cb0c08ca813c1082dfff33a91a8908107565
Ayush Sharma <ayushsharma3910@gmail.com>
2020-05-15 00:27:29 +0530

    initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() throws InterruptedException
+	{
+		driver=new ChromeDriver();
+		
+	}
+	@Test
+	public void doLogin() throws InterruptedException
+	{
+		
+		driver.get("https://gmail.com");
+		Thread.sleep(10000);
+		driver.findElement(By.id("identifierId")).sendKeys("ayushsharma09@gmail.com");
+		driver.findElement(By.xpath("//span[text()='Next']")).click();
+		
+	}
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
